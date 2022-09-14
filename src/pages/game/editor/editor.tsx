fd8bf34a058df13969deb98cfb4a2666db38360d
Ilya Stalmahov <ilyastalmahov@gmail.com>
2022-09-14 13:04:23 +0300

    lower position refresh rate

--- src/pages/game/editor/editor.tsx
+++ src/pages/game/editor/editor.tsx
@@ -1,12 +1,11 @@
 import { VStack } from "@chakra-ui/react";
-import { toJS } from "mobx";
 import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import * as Tone from "tone";
 import { Transport } from "tone";
 import { isErrorResponse } from "../../../api/config";
-import { getCurrentTime, getSong, sendSong } from "../../../api/rooms";
+import { getCurrentTime, getSong } from "../../../api/rooms";
 import { useSocketHandlers } from "../../../socket/use-socket-handlers";
 import { audioUploader } from "../../../store/audioUploader";
 import { editor, EditorMode } from "../../../store/editor";
@@ -15,7 +14,6 @@ import { Sample } from "../../../store/sample";
 import { socketStore } from "../../../store/socket";
 import { sound } from "../../../store/sound";
 import tracks from "../../../store/tracks";
-import { renderAudio } from "../../../utils/renderAudio";
 import { useEditorModeHotkey } from "../../../utils/useEditorModeHotkey";
 import { BottomMenu } from "./bottom-menu/bottom-menu";
 import { EditorDropArea } from "./editor-drop-area";
@@ -24,11 +22,13 @@ import { EditorUploadOverlay } from "./editor-upload-overlay";
 import { EditorHeader } from "./header/editor-header";
 import { EditorWorkArea } from "./track-samples/editor-work-area";
 
+export const POSITION_REFRESH_RATE = 8;
+
 export const Editor = observer(() => {
   useEffect(() => {
     const interval = setInterval(() => {
       editor.set({ playPosition: Transport.seconds });
-    }, 16);
+    }, 1000 / POSITION_REFRESH_RATE);
 
     return () => clearInterval(interval);
   }, []);
--- src/pages/game/editor/timeline/editor-play-pointer.tsx
+++ src/pages/game/editor/timeline/editor-play-pointer.tsx
@@ -1,27 +1,32 @@
 import { Box } from "@chakra-ui/react";
 import { observer } from "mobx-react-lite";
+import { CSSProperties } from "react";
 import { editor } from "../../../../store/editor";
+import { POSITION_REFRESH_RATE } from "../editor";
+
+const pointerStyle: CSSProperties = {
+  width: "0",
+  height: "0",
+  borderStyle: "solid",
+  borderWidth: "6px 6px 0 6px",
+  transform: "translate(-50%, 0)",
+  borderColor: "#ffffff transparent transparent transparent",
+};
 
 export const EditorPlayPointer = observer(() => {
+  const style: CSSProperties = {
+    width: "1px",
+    background: "white",
+    height: "100%",
+    position: "absolute",
+    zIndex: 2,
+    transition: `left ${1 / POSITION_REFRESH_RATE}s linear`,
+  };
+  style.left = `${editor.playPositionPixels}px`;
+
   return (
-    <Box
-      w={"1px"}
-      bg="white"
-      h={"full"}
-      position="absolute"
-      zIndex={10}
-      left={`${editor.playPositionPixels}px`}
-    >
-      <div
-        style={{
-          width: "0",
-          height: "0",
-          borderStyle: "solid",
-          borderWidth: "6px 6px 0 6px",
-          transform: "translate(-50%, 0)",
-          borderColor: "#ffffff transparent transparent transparent",
-        }}
-      ></div>
-    </Box>
+    <div style={style}>
+      <div style={pointerStyle}></div>
+    </div>
   );
 });
